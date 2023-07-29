888ed152d35c149d7e9edee0ca4ab922b137475c
kelvin <tearteamoguy@gmail.com>
2023-07-30 01:48:21 +0700

    add comments

--- myapp/EmailRESTAPI/views.py
+++ myapp/EmailRESTAPI/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 
-
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
@@ -9,35 +8,39 @@ from rest_framework import status
 from .models import EmailModel
 from .Serializer import EmailSerializer
 
-
-
-
-
 # Create your views here.
+
+# API view for the home page
 class HomeView(APIView):
     def get(self, request):
         return Response({'message':'Welcome to RESTFUL API project by Kelvin'})
     
 
-    
+# API view for handling EmailModel instances
 class EmailView(APIView):
-    def get(self, request,pk=None):
+    # GET request to fetch all EmailModel instances or a specific one by primary key (pk)
+    def get(self, request, pk=None):
         if pk:
             try:
-                EmailView= EmailModel.objects.get(pk=pk)
-                serializer= EmailSerializer(EmailView)
+                # Fetch the EmailModel instance with the given primary key (pk)
+                email_instance = EmailModel.objects.get(pk=pk)
+                serializer = EmailSerializer(email_instance)
                 return Response(serializer.data)
             except EmailModel.DoesNotExist:
+                # If EmailModel instance with the given pk does not exist, return an error message
                 return Response({'message':'Email model not found'})
         else:
-            EmailView=EmailModel.objects.all()
-            serializer=EmailSerializer(EmailView,many=True)
+            # Fetch all EmailModel instances and serialize them to send as a response
+            email_instances = EmailModel.objects.all()
+            serializer = EmailSerializer(email_instances, many=True)
             return Response(serializer.data)
     
-    def post(self,request):
-        serializer=EmailSerializer(data=request.data)
+    # POST request to create a new EmailModel instance
+    def post(self, request):
+        serializer = EmailSerializer(data=request.data)
         if serializer.is_valid():
+            # If the data is valid, save the new EmailModel instance and return its data in the response
             serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-    
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        # If the data is not valid, return an error response
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
